c01d3975abc9a7f65e4754414c0de4d18ad06994
GabrielNatucci <gabriel.natuccirusso@hotmail.com>
2023-12-21 19:54:02 -0300

    Adicao de um atalho para separar janelas
    - c-s para nova janela horizontal
    - c-g para nova janela vertical

--- mappings.lua
+++ mappings.lua
@@ -78,8 +78,9 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+    -- ATALHOS PARA SEPRACAO DE JANELA
+    ["<C-s>"] = { ":split<cr>", desc = "New Horizontal Window" },
+    ["<C-g>"] = { ":vsplit<cr>", desc = "New Vertical Window" },
   },
   t = {
     -- setting a mapping to false will disable it
